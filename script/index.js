dfad40cf36a7a48d4abd96f1b4d54a9d79de9501
Omar <omar.cavazos92@gmail.com>
2022-10-05 18:07:15 -0500

    metodos y propiedades de los strings .length,  .substring, .slice, .substr

--- script/index.js
+++ script/index.js
@@ -1,27 +1,41 @@
-function sumar(n1, n2){
-    return n1 + n2;
+let saludar = "hola como estas?";
+
+// for (let i = 0; i < saludar.length; i++) {
+//     console.log(saludar[i]);
+// }
+
+let copia = saludar.slice(5, 9);
+let copia2 = saludar.slice(-16,-12)
+
+function slice(str, start = 0, end = str.length) {
+    let copia=""
+    for (let i = start; i < end; i++) {
+    copia+= str[i]
+    }
+    return copia
 }
 
-function matematica (n1, n2) {
-    return sumar(n1, n2)
+let sub = saludar.substring()
+sub=saludar.substring(5, 6)
+sub=saludar.substring(-5, 6)
+sub=saludar.substring(-6, -1)
+
+function substring(str, start = 0, end = str.length) {
+    let copia=""
+    if (start < 0) start = 0
+    if (end > str.length) end = str.length
+    if (end < 0) return copia
+    for (let i = start; i < end; i++) {
+    copia+= str[i]
+    }
+    return copia
 }
 
-function restar(n1, n2){
-    return n1-n2
-}
+let subs= saludar.substr
 
-function math(fn, n1, n2){
-    return fn(n1, n2)
-}
 
-function areaCircle(r){
-    return Math.PI*r**2
-}
-
-function vol(r, h, fn){
-    return fn(r)*h
-}
-
-function vol2(r, h){
-    return Math.PI*r**2*h
+function substr(str, start = 0, end = str.length) {
+    let copia=""
+    if (end != 0) end += start
+    return substring(str, start, end)
 }
