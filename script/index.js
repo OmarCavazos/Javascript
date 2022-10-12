89a6478e4a01341d57424331c80a005864a9b008
Omar <omar.cavazos92@gmail.com>
2022-10-12 18:02:52 -0500

    sort, objetos, this

--- script/index.js
+++ script/index.js
@@ -1,41 +1,69 @@
-let saludar = "hola como estas?";
+let arr = ["b", "c", "a"].sort()
 
-// for (let i = 0; i < saludar.length; i++) {
-//     console.log(saludar[i]);
-// }
+let menorAMayor = [1, 2, 10, 4, 3].sort((a, b) => a - b)
+let mayorAMenor = [1, 2, 10, 4, 3].sort((a, b) => b - a)
 
-let copia = saludar.slice(5, 9);
-let copia2 = saludar.slice(-16,-12)
+// console.log(menorAMayor);
+// console.log(mayorAMenor);
 
-function slice(str, start = 0, end = str.length) {
-    let copia=""
-    for (let i = start; i < end; i++) {
-    copia+= str[i]
+//----------------------------------------------------------
+
+
+const obj = {}
+
+const alumno1 ={
+    nombre: "Brayan",
+    apellido: "Martinez",
+    edad: 20,
+    matricula: true,
+    cursos: ["mate", "progra"],
+    notas: [1, 2, 3, 4, 5, 6],
+}
+
+const alumno2 ={
+    nombre: "Kevin",
+    apellido: "Perez",
+    edad: 22,
+    matricula: true, 
+    cursos: ["mate", "progra", ["idioma"], {turno: "matutino"}],
+    notas: [1, 2, 3, 4, 5, 6],
+    promedio: function(){
+
     }
-    return copia
 }
 
-let sub = saludar.substring()
-sub=saludar.substring(5, 6)
-sub=saludar.substring(-5, 6)
-sub=saludar.substring(-6, -1)
+let nombre = alumno1.nombre
+let curso1 = alumno1.cursos[1]
 
-function substring(str, start = 0, end = str.length) {
-    let copia=""
-    if (start < 0) start = 0
-    if (end > str.length) end = str.length
-    if (end < 0) return copia
-    for (let i = start; i < end; i++) {
-    copia+= str[i]
-    }
-    return copia
+let nombre2 = alumno2['nombre']
+let curso2 = alumno2 ['cursos'][3]
+
+alumno2.nombre = "Kevin"
+alumno2.promedio = "Hola"
+
+// console.table(alumno1)
+// console.table(arr)
+
+
+let Kevin = alumno2
+
+// Kevin["estudia"]=true
+Kevin.estudia=false
+
+//--------------------------------------------------------
+
+const persona = {
+    nombre: "Brandon",
+    apellido: "Marlon",
+    nombreCompleto: function () {
+        return `${this.nombre} ${this.apellido}`
+    },
+    quienEsThis: () => this,
+    this: this,
+    // this2: function () {
+    //     function(){
+            // return this
+    //     }
+    // }
 }
 
-let subs= saludar.substr
-
-
-function substr(str, start = 0, end = str.length) {
-    let copia=""
-    if (end != 0) end += start
-    return substring(str, start, end)
-}
